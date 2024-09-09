a7a9b34237f50f57a8a3f478696e35ac8ea2df3b
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-09 14:50:19 +0200

    5-beturend genABC()

--- 1-ismetles/1-vezerles_tombok_objektumok/5-beturend.js
+++ 1-ismetles/1-vezerles_tombok_objektumok/5-beturend.js
@@ -1,3 +1,16 @@
 // String.fromCharCode(65) === "A"
 // "a".charCodeAt() === 97
 
+function genABC() {
+    let abc = [];
+    for (let i = 97; i < 97 + 26; i++) {
+        abc.push(String.fromCharCode(i));
+    }
+    return abc;
+}
+
+let abc = ['a', 'á', 'b', 'c', 'd', 'e', 'é', 'f', 'g', 'h', 'i', 'í', 'j', 'k', 'l', 'm', 'n', 'o', 'ó', 'ö', 'ő', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'ü', 'ű', 'v', 'w', 'x', 'y', 'z'];
+
+function hasonlit(a, b) {
+    // ???
+}
