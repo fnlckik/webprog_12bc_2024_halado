a685446ea7444fd462031fad74a488db266e012a
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-30 12:54:32 +0200

    12-pontok

--- 1-ismetles/1-math/12-pontok.js
+++ 1-ismetles/1-math/12-pontok.js
@@ -9,6 +9,17 @@ function generatePoints() {
     return pontok;
 }
 
+// a) Kiválogatás tétel
+function a(pontok) {
+    const pozitivak = [];
+    for (const pont of pontok) {
+        if (pont.x > 0 && pont.y > 0) {
+            pozitivak.push(pont);
+        }
+    }
+    return pozitivak;
+}
+
 // b) Megszámolás tétel
 function b(pontok) {
     let db = 0;
@@ -23,3 +34,29 @@ function b(pontok) {
 
 // Tesztelés
 // b(generatePoints());
+
+// c) Minimumkiválasztás tétel
+function c(pontok) {
+    let closest = pontok[0];
+    const origo = {x: 0, y: 0};
+    for (const pont of pontok) {
+        if (distance(pont, origo) < distance(closest, origo)) {
+            closest = pont;
+        }
+    }
+    return closest;
+}
+
+// d) Eldöntés tétel => Keresés tétel
+function d(pontok) {
+    let i = 0;
+    while (i < pontok.length && !(pontok[i].x + pontok[i].y === 42)) {
+        i++;
+    }
+    if (i < pontok.length) {
+        return pontok[i];
+    } else {
+        return {};
+    }
+}
+
