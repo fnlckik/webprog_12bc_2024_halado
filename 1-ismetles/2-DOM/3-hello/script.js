20fb5b62278dd984f8b74d412af6aa6271ed1526
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-11 11:30:48 +0200

    3-hello (greeting)

--- 1-ismetles/2-DOM/3-hello/script.js
+++ 1-ismetles/2-DOM/3-hello/script.js
@@ -1,25 +1,34 @@
-function handleClick() {
-    const name = document.querySelector("input").value;
+function greetUser(name, greeting) {
     const oldDiv = document.querySelector("#old");
-    oldDiv.innerHTML = `<p>Szia ${name}!</p>`;
+    oldDiv.innerHTML = `<p>${greeting} ${name}!</p>`;
 
     const newDiv = document.querySelector("#new");
     newDiv.style.display = "none";
+}
+
+function handleClick() {
+    const name = document.querySelector("input").value;
+
+    // const select = document.querySelector("select");
+    // const options = document.querySelectorAll("option");
+    // console.log(options[select.value - 1].innerText);
+    const select = document.querySelector("select");
+    const greeting = select[select.selectedIndex].innerText;
+
+    greetUser(name, greeting);
 
     localStorage.setItem("name", name);
+    localStorage.setItem("greeting", greeting);
 }
 const button = document.querySelector("button");
 button.addEventListener("click", handleClick);
 
 function handleLoad() {
     const name = localStorage.getItem("name");
-    if (name === null) {
-        return;
+    const greeting = localStorage.getItem("greeting");
+    // name === null
+    if (name) {
+        greetUser(name, greeting);
     }
-    const oldDiv = document.querySelector("#old");
-    oldDiv.innerHTML = `<p>Szia ${name}!</p>`;
-
-    const newDiv = document.querySelector("#new");
-    newDiv.style.display = "none";
 }
 window.addEventListener("load", handleLoad);
