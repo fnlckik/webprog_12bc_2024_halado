d3bcdfac3bcbf49f0c0fd7c17f7df12686a22093
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-11 11:31:23 +0200

    3-hello (delete code comments)

--- 1-ismetles/2-DOM/3-hello/script.js
+++ 1-ismetles/2-DOM/3-hello/script.js
@@ -1,17 +1,12 @@
 function greetUser(name, greeting) {
     const oldDiv = document.querySelector("#old");
     oldDiv.innerHTML = `<p>${greeting} ${name}!</p>`;
-
     const newDiv = document.querySelector("#new");
     newDiv.style.display = "none";
 }
 
 function handleClick() {
     const name = document.querySelector("input").value;
-
-    // const select = document.querySelector("select");
-    // const options = document.querySelectorAll("option");
-    // console.log(options[select.value - 1].innerText);
     const select = document.querySelector("select");
     const greeting = select[select.selectedIndex].innerText;
 
@@ -26,7 +21,6 @@ button.addEventListener("click", handleClick);
 function handleLoad() {
     const name = localStorage.getItem("name");
     const greeting = localStorage.getItem("greeting");
-    // name === null
     if (name) {
         greetUser(name, greeting);
     }
