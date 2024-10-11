b6d3e58790c3a8b00cd5496ca5a1a9f90346bdd3
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-11 12:15:36 +0200

    3-hello

--- 1-ismetles/2-DOM/3-hello/script.js
+++ 1-ismetles/2-DOM/3-hello/script.js
@@ -1,28 +1,31 @@
-function greetUser(name, greeting) {
+// JSON: JavaScript Object Notation
+
+function greetUser(user) {
     const oldDiv = document.querySelector("#old");
-    oldDiv.innerHTML = `<p>${greeting} ${name}!</p>`;
+    oldDiv.innerHTML = `<p>${user.greeting} ${user.name}!</p>`;
     const newDiv = document.querySelector("#new");
     newDiv.style.display = "none";
 }
 
-function handleClick() {
+function getUser() {
     const name = document.querySelector("input").value;
     const select = document.querySelector("select");
     const greeting = select[select.selectedIndex].innerText;
+    return { name, greeting };
+}
 
-    greetUser(name, greeting);
-
-    localStorage.setItem("name", name);
-    localStorage.setItem("greeting", greeting);
+function handleClick() {
+    const user = getUser();
+    greetUser(user);
+    localStorage.setItem("user", JSON.stringify(user));
 }
 const button = document.querySelector("button");
 button.addEventListener("click", handleClick);
 
 function handleLoad() {
-    const name = localStorage.getItem("name");
-    const greeting = localStorage.getItem("greeting");
-    if (name) {
-        greetUser(name, greeting);
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+        greetUser(user);
     }
 }
 window.addEventListener("load", handleLoad);
