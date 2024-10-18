de781fb19aa1ed33759db8d652d48ca61f96f267
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-18 12:25:31 +0200

    Oszthatosag generate(), showTable(), handleLoad()

--- 1-ismetles/3-tablazatok/Oszthatosag/script.js
+++ 1-ismetles/3-tablazatok/Oszthatosag/script.js
@@ -1,18 +1,42 @@
+// HTML table <=> JS matrix
+let m = [];
+
 function randint(a, b) {
     return Math.floor(Math.random() * (b-a+1)) + a;
 }
 
-function generate() {
+function showTable() {
     const table = document.querySelector("table");
     let data = "";
     for (let i = 0; i < 10; i++) {
         data += "<tr>";
         for (let j = 0; j < 10; j++) {
-            data += `<td>${randint(10, 99)}</td>`;
+            data += `<td>${m[i][j]}</td>`;
         }
         data += "</tr>";
     }
     table.innerHTML = data;
 }
+
+function generate() {
+    m = []; // m.splice(0, m.length); // m = [];
+    for (let i = 0; i < 10; i++) {
+        const row = [];
+        for (let j = 0; j < 10; j++) {
+            const r = randint(10, 99);
+            row.push(r);
+        }
+        m.push(row);
+    }
+    localStorage.setItem("numbers", JSON.stringify(m));
+    showTable();
+}
 const button = document.querySelector("button");
 button.addEventListener("click", generate);
+
+function handleLoad() {
+    m = JSON.parse(localStorage.getItem("numbers"));
+    if (!m) return;
+    showTable();
+}
+window.addEventListener("load", handleLoad);
