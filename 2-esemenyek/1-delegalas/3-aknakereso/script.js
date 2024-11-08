75d38b316dea247320d2c2f35f843ae3e6028ed2
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-08 12:02:35 +0100

    3-akna startGame

--- 2-esemenyek/1-delegalas/3-aknakereso/script.js
+++ 2-esemenyek/1-delegalas/3-aknakereso/script.js
@@ -1,6 +1,6 @@
 const n = 12;
 const mineCount = 10;
-let board = [];
+let board = []; // field = {value: ?2?, isMine: true/false}
 let revealedCount = 0;
 
 const table = document.querySelector("table");
@@ -11,3 +11,21 @@ function randint(a, b) {
     return Math.floor(Math.random() * (b-a+1)) + a;
 }
 
+// Feltölt egy nxn-es mátrixot mezőkkel
+// field = {value: 0, isMine: false}
+function createBoard() {
+    
+}
+
+// A mátrix alapján megjelenít egy táblázatot
+// a játékos számára!
+function showBoard() {
+    
+}
+
+function startGame() {
+    createBoard();
+    showBoard();
+}
+const button = document.querySelector("button");
+button.addEventListener("click", startGame);
