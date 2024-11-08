b8504907cd33982d40bb81ceffcff4a20a39e3c6
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-08 12:21:26 +0100

    createBoard, showBoard

--- 2-esemenyek/1-delegalas/3-aknakereso/script.js
+++ 2-esemenyek/1-delegalas/3-aknakereso/script.js
@@ -11,21 +11,43 @@ function randint(a, b) {
     return Math.floor(Math.random() * (b-a+1)) + a;
 }
 
-// Feltölt egy nxn-es mátrixot mezőkkel
+// Feltölt egy nxn-es mátrixot mezőkkel => board
 // field = {value: 0, isMine: false}
 function createBoard() {
-    
+    for (let i = 0; i < n; i++) {
+        const row = [];
+        for (let j = 0; j < n; j++) {
+            const field = {
+                value: 0,
+                isMine: false
+            };
+            row.push(field);
+        }
+        board.push(row);
+    }
 }
 
 // A mátrix alapján megjelenít egy táblázatot
 // a játékos számára!
 function showBoard() {
-    
+    const table = document.querySelector("table");
+    table.innerHTML = "";
+    for (let i = 0; i < n; i++) {
+        const tr = document.createElement("tr");
+        for (let j = 0; j < n; j++) {
+            const td = document.createElement("td");
+            const field = board[i][j];
+            td.innerText = field.value;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
 }
 
 function startGame() {
     createBoard();
     showBoard();
+    button.removeEventListener("click", startGame);
 }
 const button = document.querySelector("button");
 button.addEventListener("click", startGame);
