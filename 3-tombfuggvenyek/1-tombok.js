fafec8ad4b9221921f6efe6bf0666ac60a506990
fnlckik <farkas.norbert@ckik.hu>
2024-12-06 11:51:40 +0100

    1-tombok.js

--- 3-tombfuggvenyek/1-tombok.js
+++ 3-tombfuggvenyek/1-tombok.js
@@ -10,14 +10,28 @@ console.log("Módosítások:", a);
 
 // 1. Egyesítsük a tömböket!
 const b = [3, 4, 5];
-const c = [];
-const d = [];
+const c = a.concat(b); // [11, 2, 7, 3, 4, 5]
+/*
+for (const e of a) {
+    c.push(e);
+}
+for (const e of b) {
+    c.push(e);
+}
+*/
+const d = [...a, ...b];
 console.log("Összefésülve:", c, d);
 
 // 2. Résztömb: 2. elemtől 4. elemig (1. indextől 3. indexig)
-const e = [];
+const e = c.slice(1, 4); // [2, 7, 3]
 console.log("Résztömb:", e);
 
 // 3. Szöveggé alakítás: elválasztó szóköz legyen
-const f = [];
+const f = c.join(" "); // "11 2 7 3 4 5"
+/*
+const f = "";
+for (const e of c) {
+    f += e + " ";
+}
+*/
 console.log("Szöveggé:", f);
