f597c38b3b3410ebdb04043eafb2789f85b976fd
farkas.norbert <farkas.norbert@ckik.hu>
2024-12-15 15:13:06 +0100

    3-emberek.js (megoldas)

--- 3-tombfuggvenyek/3-emberek.js
+++ 3-tombfuggvenyek/3-emberek.js
@@ -1,3 +1,4 @@
+// 12 fő
 const people = [
     { name: "Dani", age: 47 },
     { name: "Kata", age: 31 },
@@ -13,4 +14,48 @@ const people = [
     { name: "Ákos", age: 29 }
 ];
 
-console.log("F1:", people);
+// F1
+console.log("F1:", people.find(p => p.age < 20));
+
+// F2
+console.log("F2:", people.filter(p => p.age < 40).length);
+
+// F3
+// console.log("F3:", !people.some(p => p.name.length > 6));
+// Minden ember neve max 6 karakter
+console.log("F3:", people.every(p => p.name.length <= 6));
+
+// F4
+// console.log("F4:", people.toSorted((a, b) => a.age - b.age));
+function compare(p1, p2) {
+    if (p1.age < p2.age) {
+        return -1;
+    } else if (p1.age > p2.age) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+console.log("F4:", people.toSorted(compare));
+
+// F5
+const atlag = people.reduce((s, p) => s + p.age, 0) / people.length;
+console.log("F5:", (atlag).toFixed(2));
+
+// F6
+console.log("F6:", people.filter(p => p.age > atlag).map(p => p.name));
+
+// F7
+console.log("F7:", people.findLast(p => p.name[0] === "J").name);
+
+// F8
+console.log("F8:", people.reduce((m, p) => p.age > m.age ? p : m, people[0]));
+
+// F9
+console.log("F9:", people.filter((p1, i) => people.some((p2, j) => i !== j && p1.age === p2.age)));
+
+// F10
+function hasA(name) {
+    return Array.from(name).some(c => c.toUpperCase() === "A");
+}
+console.log("F10:", people.filter(p => hasA(p.name)).map(p => p.name));
