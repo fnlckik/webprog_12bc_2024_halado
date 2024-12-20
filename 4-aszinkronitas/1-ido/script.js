845af68cf466297b1ebcd49eb077f112748d10f6
farkas.norbert <farkas.norbert@ckik.hu>
2024-12-20 10:47:39 +0100

    Date objektum (year, month, date)

--- 4-aszinkronitas/1-ido/script.js
+++ 4-aszinkronitas/1-ido/script.js
@@ -7,3 +7,13 @@ button.addEventListener("click", increase);
 
 // ----------------------------------
 
+const timestamp = Date.now(); // Hány ms (ezredmásodperc) telt el 1970. 01. 01. óta? ~kb. statikus metódus
+const today = new Date(); // példányosítás, meghívjuk a konstruktort
+const ww2 = new Date("1939-09-01"); // "polimorfizmus"
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1; // 0-tól 11-ig
+const day = today.getDate();
+const weekday = today.getDay(); // 0 - Vasárnap, 1 - Hétfő, ... 
+
+console.log(year, month, day);
