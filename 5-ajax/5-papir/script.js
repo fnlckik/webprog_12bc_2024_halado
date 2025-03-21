782810c964eab31a304e6bb0a5238bef5a6d4758
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-21 12:19:04 +0100

    papir - POST

--- 5-ajax/5-papir/script.js
+++ 5-ajax/5-papir/script.js
@@ -17,14 +17,67 @@ keresButton.addEventListener("click", getData);
 
 // -----------------------------------------------------
 // Adatok küldése
+// const adat = {
+//     osztaly: osztaly,
+//     tomeg: tomeg
+// };
 const kuldesButton = document.querySelector("#kuldes");
-function sendDataFetch() {
+async function sendDataFetch() {
     const osztaly = document.querySelector("#osztaly-kuldes").value;
     const tomeg = document.querySelector("#tomeg-kuldes").value;
+    const adat = { osztaly, tomeg };
+    const OPTIONS = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/xml"
+        },
+        body: JSON.stringify(adat)
+    };
+    try {
+        const response = await fetch("http://localhost/papir/create/", OPTIONS);
+        const data = await response.json();
+        if (!response.ok) {
+            // throw new Error("Hibás kérés! Hiányzó adat!");
+            throw new Error(data.hiba);
+        }
+        console.log(data);
+    } catch (error) {
+        // console.error(error.message);
+        console.clear();
+        console.warn(error.message);
+        const p = document.createElement("p");
+        p.innerHTML = error.message;
+        const hr = document.querySelector("hr");
+        document.querySelector("body").insertBefore(p, hr);
+    }
 }
+
+// function handleLoad(xhr) {
+//     const data = JSON.parse(xhr.response);
+//     console.log(data);
+// }
 
 function sendDataXHR() {
     const osztaly = document.querySelector("#osztaly-kuldes").value;
     const tomeg = document.querySelector("#tomeg-kuldes").value;
+    const adat = { osztaly, tomeg };
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+        try {
+            if (xhr.status === 400) {
+                throw new Error("Hibás / hiányos adatokat küldtél!");
+            }
+            const data = JSON.parse(xhr.response);
+            console.log(data);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    xhr.onerror = () => {
+        console.log("Nem fut a szerver!");
+    }
+    // xhr.addEventListener("load", () => {handleLoad(xhr)});
+    xhr.open("POST", "http://localhost/papir/create/");
+    xhr.send(JSON.stringify(adat));
 }
 kuldesButton.addEventListener("click", sendDataXHR);
