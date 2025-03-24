8c471f12ab4374ae21b62a7fd9da86b694a1760d
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-24 13:06:44 +0100

    6-reqres (xhr)

---
+++ 5-ajax/6-reqres/script.js
@@ -0,0 +1,32 @@
+const button = document.querySelector("button");
+async function sendDataFetch() {
+    const name = document.querySelector("#name").value;
+    const job = document.querySelector("#job").value;
+    const user = { name, job };
+    const OPTIONS = {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+    const response = await fetch("https://reqres.in/api/users", OPTIONS);
+    const data = await response.json();
+    console.log(data);
+}
+
+async function sendDataXHR() {
+    const name = document.querySelector("#name").value;
+    const job = document.querySelector("#job").value;
+    const user = { name, job };
+    // console.log(user);
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://reqres.in/api/users");
+    xhr.onload = () => {
+       const data = JSON.parse(xhr.response);
+       console.log(data);
+    };
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(user));
+}
+button.addEventListener("click", sendDataXHR);
